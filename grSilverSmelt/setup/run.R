916fa0551a03df5394536cfbf6ef8684f1fd59cc
Paul Frater <pnfrater@gmail.com>
2016-06-23 13:29:23 +0000

    Added Autumn grounfish surveys - forgot run.R file

--- grSilverSmelt/setup/run.R
+++ grSilverSmelt/setup/run.R
@@ -2,10 +2,11 @@ library(Rgadget)
 setwd('~/gadget/gadget-models/grSilverSmelt/gssModel')
 tmp <- gadget.iterative(rew.sI=TRUE,
                         main='main',
-                        grouping=list(sind=c('si.1525', 'si.2540','si.4055'),
-                                      survey=c('ldist.igfs', 'aldist.igfs'),
-                                      comm=c('ldist.bmt', 'aldist.bmt',
-                                             'ldist.pgt', 'aldist.pgt', 
-                                             'ldist.other', 'aldist.other')),
+                        grouping=list(sind=c('igfs.si.1525', 'igfs.si.2540',
+                                             'igfs.si.4055', 'aut.si.1525',
+                                             'aut.si.2540', 'aut.si.4055'),
+                                      survey=c('ldist.igfs', 'aldist.igfs',
+                                               'ldist.aut', 'aldist.aut'),
+                                      comm=c('ldist.bmt', 'aldist.bmt')),
                         wgts='WGTS')
 
